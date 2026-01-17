629b39748408018474f53a504ce4583704e16049
mattc7660 <mdcking4749@live.com>
2026-01-17 16:54:02 -0500

    events for each button

--- webpage.js
+++ webpage.js
@@ -4,6 +4,9 @@ const startScreen = document.getElementById("startScreen")
 const gameScreen = document.getElementById("gameScreen")
 const startButton = document.getElementById("startButton")
 const backButton = document.getElementById("backButton")
+const rockButton = document.getElementById("rockButton")
+const paperButton = document.getElementById("paperButton")
+const scissorsButton = document.getElementById("scissorsButton")
 
 
 // start button switches to game screen
@@ -20,3 +23,21 @@ backButton.addEventListener("click", function()
     gameScreen.style.display = "none";
     startScreen.style.display = "block";
 });
+
+// options buttons
+rockButton.addEventListener("click", function()
+{
+    console.log("rock");
+});
+
+paperButton.addEventListener("click", function()
+{
+    console.log("paper");
+});
+
+scissorsButton.addEventListener("click", function()
+{
+    console.log("scissors");
+});
+
+
